872bf43ccd371da840307b57347ba7e6d1751b76
ducquoc <ducquocvyy@gmail.com>
2025-11-28 14:10:40 +0700

    Hot fix validate business #139

--- src/main/java/com/EIPplatform/exception/errorCategories/BusinessDetailError.java
+++ src/main/java/com/EIPplatform/exception/errorCategories/BusinessDetailError.java
@@ -5,12 +5,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 
 public enum BusinessDetailError implements ErrorCodeInterface {
-    TAX_CODE_DUPLICATE(1001, "Tax code {0} already exists", HttpStatus.BAD_REQUEST),
-    BUSINESS_REGISTRATION_NUMBER_DUPLICATE(1002, "Business registration number {0} already exists", HttpStatus.BAD_REQUEST),
-    PHONE_NUMBER_DUPLICATE(1003, "Phone number {0} already exists", HttpStatus.BAD_REQUEST),
-    NOT_FOUND(1004, "Business not found for user account ID {0}", HttpStatus.NOT_FOUND),
-    BUSINESS_DETAIL_ID_NOT_FOUND(1005, "Business detail with ID {0} not found", HttpStatus.NOT_FOUND),
-    ISO_CERT_FILE_NOT_FOUND(1006, "ISO certificate file for business detail ID {0} not found", HttpStatus.NOT_FOUND);
+    TAX_CODE_DUPLICATE(1001, "Tax code already exists", HttpStatus.BAD_REQUEST),
+    BUSINESS_REGISTRATION_NUMBER_DUPLICATE(1002, "Business registration number already exists", HttpStatus.BAD_REQUEST),
+    PHONE_NUMBER_DUPLICATE(1003, "Phone number already exists", HttpStatus.BAD_REQUEST),
+    NOT_FOUND(1004, "Business not found for the provided user account ID", HttpStatus.NOT_FOUND),
+    BUSINESS_DETAIL_ID_NOT_FOUND(1005, "Business detail not found for the provided ID", HttpStatus.NOT_FOUND),
+    ISO_CERT_FILE_NOT_FOUND(1006, "ISO certificate file not found for the provided business detail ID", HttpStatus.NOT_FOUND);
+
 
     private final int code;
     private final String message;
